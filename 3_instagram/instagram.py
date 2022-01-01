c5c8ae97f9acadf59731beb398b86afe21cc69ff
Yashkumar <jainy6214@gmail.com>
2022-01-01 12:42:00 +0530

    enters username and pass

--- 3_instagram/instagram.py
+++ 3_instagram/instagram.py
@@ -1,6 +1,6 @@
 from instabot import Bot
 bot = Bot()
-bot.login(username="", password="")
+bot.login(username="pyinsta_bot_99", password="PythonProject")
 
 ######  upload a picture #######
 bot.upload_photo("yoda.jpg", caption="biscuit eating baby")
